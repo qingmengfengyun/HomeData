382a227bdb2ee3dd1d48ce37827e90532d7d558f
qingmengfengyun <qingmengfengyun@sina.com>
2024-04-07 14:17:14 +0800

    修改nvim配置

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -19,8 +19,10 @@ keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
 keymap.set("n", "<leader>nh", ":nohl<CR>")
 
 -- 切换buffer
-keymap.set("n", "<C-L>", ":bnext<CR>")
-keymap.set("n", "<C-H>", ":bprevious<CR>")
+-- keymap.set("n", "<C-L>", ":bnext<CR>")
+keymap.set("n", "<leader>l", ":bnext<CR>")
+-- keymap.set("n", "<C-H>", ":bprevious<CR>")
+keymap.set("n", "<leader>h", ":bnext<CR>")
 
 -- ---------- 插件 ---------- ---
 -- nvim-tree
